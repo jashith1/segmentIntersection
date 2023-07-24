fc48930a3e8680d0cd007954591cffa8444527ce
bloppai <bloppai@bloppai.attlocal.net>
2023-07-24 18:23:03 -0500

    fix segment intersection

--- index.js
+++ index.js
@@ -23,11 +23,24 @@ const D = {
 	y: 200,
 };
 
-let t = 0;
+const mouse = {
+	x: 0,
+	y: 0,
+};
+document.onmousemove = (event) => {
+	mouse.x = event.x;
+	mouse.y = event.y;
+};
 
 animate();
 
 function animate() {
+	const radius = 50;
+	A.x = mouse.x;
+	A.y = mouse.y - radius;
+	B.x = mouse.x;
+	B.y = mouse.y + radius;
+
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	ctx.beginPath();
 	ctx.moveTo(A.x, A.y);
@@ -40,18 +53,9 @@ function animate() {
 	draw(B, 'B');
 	draw(C, 'C');
 	draw(D, 'D');
-	const M = {
-		x: lerp(A.x, B.x, t),
-		y: lerp(A.y, B.y, t),
-	};
 
-	const N = {
-		x: lerp(C.x, D.x, t),
-		y: lerp(C.y, D.y, t),
-	};
-	draw(M, 'M');
-	draw(N, 'N');
-	t += 0.005;
+	const I = getSegmentIntersection(A, B, C, D);
+	if (I) draw(I, 'I');
 	requestAnimationFrame(animate);
 }
 
@@ -71,3 +75,26 @@ function draw(point, label) {
 function lerp(A, B, t) {
 	return A + (B - A) * t;
 }
+
+function getSegmentIntersection(A, B, C, D) {
+	const top = (D.x - C.x) * (A.y - C.y) - (D.y - C.y) * (A.x - C.x);
+	const bottom = (D.y - C.y) * (B.x - A.x) - (D.x - C.x) * (B.y - A.y);
+	if (bottom === 0) return null;
+	const t = top / bottom;
+	if (t > 1 || t < 0) return null;
+	return {
+		x: lerp(A.x, B.x, t),
+		y: lerp(A.y, B.y, t),
+		offset: t,
+	};
+}
+
+/*
+Ax + (Bx-Ax) * t === Cx + (Dx - Cx) * u
+Ax - Cx + ((Bx - Ax) * t) === ((Dx - Cx) * u)
+(Dx - Cx)(Ay - Cy + (By-Ay) * t) === (Dy - Cy) * (Ax - Cx + ((Bx - Ax) * t))
+(Dx - Cx)(Ay - Cy) + (Dx - Cx)(By - Ay)(t) === (Dy - Cy)(Ax - Cx) + (Dy - Cy)(Bx - Ax)(t)
+(Dx - Cx)(Ay - Cy) - (Dy - Cy)(Ax - Cx) === (Dy - Cy)(Bx - Ax)(t) - (Dx - Cx)(By - Ay)(t) 
+(Dx - Cx)(Ay - Cy) - (Dy - Cy)(Ax - Cx) === (t)((Dy - Cy)(Bx - Ax) - (Dx - Cx)(By - Ay))
+t = ((Dx - Cx)(Ay - Cy) - (Dy - Cy)(Ax - Cx))/((Dy - Cy)(Bx - Ax) - (Dx - Cx)(By - Ay))
+*/
